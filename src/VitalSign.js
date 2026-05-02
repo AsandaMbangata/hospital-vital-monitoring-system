e30d2c7d14f50fdddcd1b3d9a60b4a29e5400759
AsandaMbangata <160546456+AsandaMbangata@users.noreply.github.com>
2026-05-02 10:17:06 +0200

    Create VitalSign class to monitor health metrics

---
+++ src/VitalSign.js
@@ -0,0 +1,13 @@
+class VitalSign {
+    constructor(bp, heartRate, temp) {
+        this.bp = bp;
+        this.heartRate = heartRate;
+        this.temp = temp;
+    }
+
+    isAbnormal() {
+        return this.heartRate > 100 || this.temp > 38;
+    }
+}
+
+module.exports = VitalSign;
