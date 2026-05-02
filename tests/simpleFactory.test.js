26d88f7054fed0d62bad5b68fee82d3ce0706c61
AsandaMbangata <160546456+AsandaMbangata@users.noreply.github.com>
2026-05-02 18:34:10 +0200

    Add test for UserFactory createUser method

--- tests/simpleFactory.test.js
+++ tests/simpleFactory.test.js
@@ -1,1 +1,6 @@
+const UserFactory = require('../creational_patterns/simpleFactory');
 
+test("create patient", () => {
+    const user = UserFactory.createUser("patient", 1, "a@mail.com");
+    expect(user.role).toBe("patient");
+});
