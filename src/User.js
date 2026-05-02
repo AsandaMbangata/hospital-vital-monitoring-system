3779365b6d1f529ff5df453dbdc54fb20727951f
AsandaMbangata <160546456+AsandaMbangata@users.noreply.github.com>
2026-05-02 10:13:47 +0200

    Add User class with constructor and login method

--- src/User.js
+++ src/User.js
@@ -1,1 +1,13 @@
+class User {
+    constructor(id, email, role) {
+        this.id = id;
+        this.email = email;
+        this.role = role;
+    }
 
+    login() {
+        return `${this.email} logged in`;
+    }
+}
+
+module.exports = User;
